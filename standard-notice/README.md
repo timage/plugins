043aad5c1e73a4ba514b80683b95833400405185
Tom McFarlin <tom@tommcfarlin.com>
2012-08-11 22:18:45 -0400

    * Updating the plugin to be based on Single Post Message * Updating the look and feel to be themed more closely with Standard * Updated the screenshot

--- standard-notice/README.md
+++ standard-notice/README.md
@@ -1,9 +1,10 @@
-# Standard Notice
+# Standard Post Notice
 
 Easily add short notices and announcements above posts. Displays on both the blog and in RSS feeds. Developed for [Standard](http://standardtheme.com) by [8BIT](http://8bit.io) but compatible with any theme.
 
 * [Tom McFarlin](http://tommcfarlin.com)
 * GPL
+* Based on [Single Post Message](http://wordpress.org/extend/plugins/single-post-message/) but tailored for Standard.
 
 ## Screenshots
 
@@ -23,7 +24,7 @@ An example of what the notice looks like on the blog. The same styles are applie
 
 ### FTP
 
-1. Extract the plugin to a directory on your computer (it should be called 'standard-notices')
+1. Extract the plugin to a directory on your computer (it should be called 'standard-notice')
 2. Upload to your wp-content/plugins directory
 
 ### WordPress Dashboard
@@ -35,6 +36,12 @@ An example of what the notice looks like on the blog. The same styles are applie
 5. Upload and install
 
 ## Changelog
+
+_1.1 (2012.08.11)_
+
+* Updating the plugin to be based on Single Post Message
+* Updating the look and feel to be themed more closely with Standard
+* Updated the screenshot
 
 _1.0.1 (2012.07.18)_
 
--- standard-notice/plugin.php
+++ standard-notice/plugin.php
@@ -1,15 +1,15 @@
 <?php
 /*
 Plugin Name: Standard Notice
-Plugin URI: http://github.com/eightbit/plugins/standard-notice
-Description: Easily add short notices and announcements above posts. Displays on both the blog and in RSS feeds.
-Version: 1.0.1
+Plugin URI: https://github.com/eightbit/plugins/tree/master/standard-notice
+Description: Easily add short messages and announcements above posts. Displays in the RSS feed and on the blog.
+Version: 1.1
 Author: Tom McFarlin
 Author URI: http://tommcfarlin.com
-Author Email: tom@8bit.io
+Author Email: tom@tommcfarlin.com
 License:
 
-  Copyright 2012 Tom McFarlin (tom@8bit.io)
+  Copyright 2012 Tom McFarlin (tom@tommcfarlin.com)
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License, version 2, as 
@@ -29,7 +29,7 @@ License:
 class Standard_Notice {
 
 	// We write these out inline so that it is also styled in RSS readers
-	private $styles = 'padding: 1em; background: #DDD; margin: 0 0 1em 0; border: 1px solid #CCC;';
+	private $styles = 'background-color:#d9edf7;border:1px solid #bce8f1;color:#3a87ad;border-color:#bce8f1;border-radius:4px;-moz-border-radius:4px;-webkit-border-radius:4px;text-shadow:0 1px 0 rgba(255, 255, 255, 0.5);font-family:Arial,Sans-serif;font-size:12px;margin-bottom:18px;margin:8px 0 8px 0;padding:12px;';
 	 
 	/*--------------------------------------------*
 	 * Constructor
@@ -44,6 +44,7 @@ class Standard_Notice {
 	
 		// Include admin styles
 		add_action( 'admin_print_styles', array( &$this, 'add_admin_styles' ) );
+		add_action( 'admin_enqueue_scripts', array( &$this, 'add_admin_scripts' ) );
 		
 		// Add the post meta box to the post editor
 		add_action( 'add_meta_boxes', array( &$this, 'add_notice_metabox' ) );
@@ -69,13 +70,23 @@ class Standard_Notice {
  	 } // end add_admin_styles
 	
 	/**
+ 	 * Introduces the admin scripts
+ 	 */
+ 	 function add_admin_scripts() {
+	 	 
+	 	 wp_register_script( 'standard-notice', plugins_url() . '/standard-notice/js/admin.min.js' );
+	 	 wp_enqueue_script( 'standard-notice' );
+	 	 
+ 	 } // end add_admin_styles
+	
+	/**
 	 * Adds the meta box below the post content editor on the post edit dashboard.
 	 */
 	 function add_notice_metabox() {
 	
 		add_meta_box(
 			'standard_notice',
-			__( 'Post Notice', 'standard-notice' ),
+			__( 'Standard Notice', 'standard-notice' ),
 			array( &$this, 'standard_notice_display' ),
 			'post',
 			'normal',
@@ -91,9 +102,21 @@ class Standard_Notice {
 		
 		wp_nonce_field( plugin_basename( __FILE__ ), 'standard_notice_nonce' );
 
-		echo '<textarea id="standard_notice" name="standard_notice">' . get_post_meta( $post->ID, 'standard_notice', true ) . '</textarea>';
+		$html = '<textarea id="standard-notice" name="standard_notice" placeholder="' . __( 'Enter your post message here. HTML accepted.', 'standard-notice' ) . '">' . get_post_meta( $post->ID, 'standard_notice', true ) . '</textarea>';
 		
-	} // end standard_notice_display
+		$html .= '<p id="standard-notice-preview">';
+			
+			if( '' == ( $post_message = get_post_meta( get_the_ID(), 'standard_notice', true ) ) ) {
+				$html .= '<span id="standard-notice-default">' . __( 'Your notice preview will appear here.', 'standard-notice' ) . '</span>'; 
+			} else {
+				$html .= $post_message;
+			} // end if/else
+			
+		$html .= '</p><!-- /#standard-notice-message-preview -->';
+		
+		echo $html;
+		
+	} // end standard_notice_message_display
 	
 	/**
 	 * Saves the notice for the given post.
@@ -121,11 +144,8 @@ class Standard_Notice {
 				} // end if
 			} // end if/else
 		
-			// Read the Link's URL
-			$link_url = '';
-			if( isset( $_POST['standard_notice'] ) ) {
-				$link_url = $_POST['standard_notice'];
-			} // end if
+			// Read the post message
+			$post_message = isset( $_POST['standard_notice'] ) ? $_POST['standard_notice'] : '';
 			
 			// If the value exists, delete it first. I don't want to write extra rows into the table.
 			if ( 0 == count( get_post_meta( $post_id, 'standard_notice' ) ) ) {
@@ -133,7 +153,7 @@ class Standard_Notice {
 			} // end if
 	
 			// Update it for this post.
-			update_post_meta( $post_id, 'standard_notice', $link_url );
+			update_post_meta( $post_id, 'standard_notice', $post_message );
 	
 		} // end if
 	
@@ -150,11 +170,11 @@ class Standard_Notice {
     	// If there is a notice, prepend it to the content
     	if( '' != get_post_meta( get_the_ID(), 'standard_notice', true ) ) {
     	
-	    	$post_notice = '<div style="' . $this->styles . '" class="standard-notice">';
-	    		$post_notice .= get_post_meta( get_the_ID(), 'standard_notice', true );
-	    	$post_notice .= '</div><!-- /.standard-notice -->';
+	    	$post_message = '<p style="' . $this->styles . '" class="standard-notice">';
+	    		$post_message .= get_post_meta( get_the_ID(), 'standard_notice', true );
+	    	$post_message .= '</p><!-- /.standard-notice -->';
 	    	
-	    	$content = $post_notice . $content;
+	    	$content = $post_message . $content;
 	    	
     	} // end if 
     	
