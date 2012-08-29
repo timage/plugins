931405dafb7504301f6ca7d4f45d8abe0d412426
Michael Novotny <manovotny@gmail.com>
2012-08-29 14:56:20 -0500

    Implementing code review suggestions for Standard Sticky Footer.

--- standard-sticky-footer/changelog.md
+++ standard-sticky-footer/changelog.md
@@ -1,5 +1,5 @@
 # Changelog
 
-## 1.0 (August 28th, 2012)
+## 1.0 (August 29th, 2012)
 
 * Initial release.
--- standard-sticky-footer/js/plugin.js
+++ standard-sticky-footer/js/plugin.js
@@ -3,77 +3,63 @@
 
 	1. Initialization
 	2. Functions
-	3. Event Handlers
-		A. Resize
 
 /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//\/\/\/\/\/\/\/\/\/\
 */
 
 
+/* Initialization
+---------------------------------------------------------------------------------- */
+		
 ( function( $ ) {
 
 	$( function() {
 		
-		/* Initialization
-		---------------------------------------------------------------------------------- */
-		
-		// get browser
-		var browser = $( window );
-		
-		// check browser
-		checkBrowserHeight();
 		
 		
-		/* Functions
-		---------------------------------------------------------------------------------- */
+		// check browser size on load and resize
+		$( window ).load( function() { 
 		
-		/**
-		 * Checks the height of the browser window.
-		 *
-		 * @since 1.0
-		 */
-		function checkBrowserHeight() {
+			checkBrowserSize( $ );
 			
-			// get wrapper
-			var wrapper = $( '#wrapper' );
-			
-			// get footer
-			var footer = $( '#footer' );
-			
-			// check if the content, including padding and margins, is not enough to fill the browser window
-			if ( browser.height() > ( wrapper.offset().top + wrapper.outerHeight( true ) + footer.height() ) ) {
-				
-				// not enough content to fill browser, stick footer
-				footer.addClass( 'stick' );
-				
-			} else {
-				
-				// enough content to fill browser, "unstick" footer
-				footer.removeClass( 'stick' );
-				
-			} // end if
-			
-		} // end check browser height
+		} )
+		.resize( function() {
 		
-		
-		/* Event Handlers
-		---------------------------------------------------------------------------------- */	
-		
-		/* Resize
-		---------------------------------------------------------------- */
-		
-		/**
-		 * Checks for browser resizing.
-		 * 
-		 * @since 1.0
-		 */
-		browser.resize( function() {
+			checkBrowserSize( $ );
 			
-			// check browser height
-			checkBrowserHeight();
-			
-		} ); // end browser resize
+		} );
 		
 	} );
 	
-} )( jQuery );
+} ( jQuery ) );
+
+
+/* Functions
+---------------------------------------------------------------------------------- */
+
+/**
+ * Checks the size of the browser window.
+ *
+ * @since 1.0
+ */
+function checkBrowserSize( $ ) {
+	
+	// get elements
+	var $browser = $( window );
+	var $footer = $( '#footer' );
+	var $wrapper = $( '#wrapper' );
+	
+	// check if the content, including padding and margins, is not enough to fill the browser window
+	if ( $browser.height() > ( $wrapper.offset().top + $wrapper.outerHeight( true ) + $footer.height() ) ) {
+		
+		// not enough content to fill browser, stick footer
+		$footer.addClass( 'stick' );
+		
+	} else {
+		
+		// enough content to fill browser, "unstick" footer
+		$footer.removeClass( 'stick' );
+		
+	} // end if
+	
+} // end check browser size
